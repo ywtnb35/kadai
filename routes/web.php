4f760077acfd0bb86063900547d2c0efd7c013f7
ywtnb35 <watsyk35@outlook.jp>
2022-12-03 03:16:58 +0000

    laravel04

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function(){
+    Route::get('news/create','add');
+});
+
+//課題３
+Route::controller(AAAController::class)->prefix('XXX')->group(function(){
+    Route::get('admin/XXX','bbb');
+});
+
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->group(function(){
+    Route::get('profile/create','add');
+    Route::get('profile/edit','edit');
+});
